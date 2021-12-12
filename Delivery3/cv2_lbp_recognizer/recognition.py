592ce4461235c27874efde85ddc72e4215160cb1
Nick <nnewton2@nd.edu>
2021-12-12 16:03:39 -0500

    tested results

--- Delivery3/cv2_lbp_recognizer/recognition.py
+++ Delivery3/cv2_lbp_recognizer/recognition.py
@@ -1,5 +1,4 @@
 import cv2, os, sys
-import numpy as np
 
 def check_path(path):
     dir = os.path.dirname(path)
@@ -41,12 +40,35 @@ def recognize_face(img_path, id_map):
 
     return result
 
-def accuracy_test(dir, detected, trained):
+def write_and_print(dir, total, correct, errors):
+
+    rate_correct = correct/total
+    rate_error = errors/total
+
+    f = open("log.txt", "a")
+    f.write("Results for directory " + str(dir) + "\n")
+    f.write("   Total images = " + str(total) + "\n")
+    f.write("   # Correct predictions = " + str(correct) + "\n")
+    f.write("   # Errors = " + str(errors) + "\n")
+    f.write("   Rate of correct identification = " + str(rate_correct) + "\n")
+    f.write("   Rate of errors = " + str(rate_error) + "\n\n")
+
+    print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Results for directory " + str(dir))
+    print("   Total images = " + str(total))
+    print("   # Correct predictions = " + str(correct))
+    print("   # Errors = " + str(errors))
+    print("   Rate of correct identification = " + str(rate_correct))
+    print("   Rate of errors = " + str(rate_error))
+
+def accuracy_test(dir, detected, model):
     id_map = build_id_map(detected)
-    face_recognizer.read(trained)
+    face_recognizer.read(model)
 
     total = 0
     correct = 0
+    errors = 0
+    print("Testing the model for directory " + dir)
 
     for name in os.listdir(dir):
         name_path = os.path.join(dir, name)
@@ -57,46 +79,50 @@ def accuracy_test(dir, detected, trained):
             img_path = os.path.join(name_path, face)
 
             result = recognize_face(img_path, id_map)
-            print("Name: " + name + " --> Result: " + result)
-            recognized_name = result.split(',')[0]
 
-            if (recognized_name == name):
-                correct += 1
+            if (result == ''):
+                print("Error with " + img_path)
+                errors += 1
+            else:
+                print("Name: " + name + " --> Result: " + result)
+                recognized_name = result.split(',')[0]
+
+                if (recognized_name == name):
+                    correct += 1
             total += 1
 
-    accuracy = correct / total
-    print("Accuracy: " + str(accuracy))
+    write_and_print(dir, total, correct, errors)
 
 def main():
 
     for arg in sys.argv[1:]:
         dir = ''
         detected = ''
-        trained = ''
+        model = ''
 
         if (arg == '-t'):
             dir = '1_training/'
-            detected = '1_detected/'
-            trained = '1_trained/model.yml'
+            detected = '1_DETECTED/'
+            model = '1_TRAINED/model.yml'
         elif (arg == '-v'):
             dir = '2_validation/'
-            detected = '2_detected/'
-            trained = '2_trained/model.yml'
+            detected = '2_DETECTED/'
+            model = '2_TRAINED/model.yml'
         elif (arg == '-u'):
             dir = '3_unknown/'
-            detected = '3_detected/'
-            trained = '3_trained/model.yml'
+            detected = '3_DETECTED/'
+            model = '3_TRAINED/model.yml'
         else:
             print('Not a valid argument')
             continue
 
-        if (check_path(dir) and check_path(detected) and check_path(trained)):
-            accuracy_test(dir, detected, trained)
+        if (check_path(dir) and check_path(detected) and check_path(model)):
+            accuracy_test(dir, detected, model)
             #result = recognize_face('2_validation/Kurt_Warner/Kurt_Warner_0001.jpg', id_map)
             #result = recognize_face('2_validation/LeBron_James/LeBron_James_0001.jpg', id_map)
             #print(result)
         else:
-            print('Assure the following directories are made: ' + detected + ' ' + trained)
+            print('Assure the following directories are made: ' + dir + ' ' + detected + ' ' + model)
 
 if __name__ == '__main__':
     main()
